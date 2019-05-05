db71926dc52ad2913097bf2fb412e09a5a4e4f8d
Eduardo Coutinho <e.g.coutinho@gmail.com>
2019-05-04 21:46:32 -0300

    Initial Commit

---
+++ ToDo List Cocoa/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ToDo List Cocoa
+//
+//  Created by Eduardo G. Coutinho on 04/05/19.
+//  Copyright © 2019 CyberShark. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
